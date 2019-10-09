e7ac904e80e06bdd336297a7590a0bcf6dc597c8
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-10-09 13:44:08 +0700

    customer restful

---
+++ src/main/java/com/codegym/repository/Repository.java
@@ -0,0 +1,14 @@
+package com.codegym.repository;
+
+import java.util.List;
+
+public interface Repository<E> {
+
+  List<E> findAll();
+
+  E findById(Long id);
+
+  void save(E model);
+
+  void remove(Long id);
+}
